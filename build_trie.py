d793064a9b5b5ba5a6ef3e2bbb371fa68468d9f2
Peter Landwehr <pmlandwehr@gmail.com>
2015-01-19 14:56:53 -0500

    Removed build_trie.py, since the code was integrated into job setup.

--- build_trie.py
+++
@@ -1,6 +0,0 @@
-from westafricatwitter import init_gazetteers
-
-if __name__ == '__main__':
-    init_gazetteers('westAfrica.csv')
-    init_gazetteers('otherPlace.csv')
-    init_gazetteers('CrisisLexRec.csv')
